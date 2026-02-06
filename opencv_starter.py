e04c90c571c04916f6041df810a779080c0188d1
B00150149 <B00150149@mytudublin.ie>
2026-02-06 12:26:33 +0000

    Lecture 2: threshold
    Threshold using cv to define the color and shape

---
+++ opencv_starter.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+import time
+
+def threshold (img, thresh):
+    for x in range(0, img.shape[0]):
+        for y in range(0, img.shape[1]):
+            if img[x,y] > thresh:
+                img[x,y] = 255
+            else:
+                img[x,y] = 0
+    return img
+
+
+for i in range(1,16):
+    #read in an image into memory
+    img = cv.imread('C:/Users/yashi/Documents/01_tud_yashika/4th_year/Computer Vision/lab1/Oring' + str(i) + '.jpg',0)
+    thresh = 50
+    bw = threshold(img, thresh)
+    rgb = cv.cvtColor(bw,cv.COLOR_GRAY2RGB)
+    cv.putText(rgb, "Image:" + str(i) , (40, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+    cv.circle(rgb, (40, 40), 20, (0,0,255))
+    cv.imshow('thresholded image', rgb)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
